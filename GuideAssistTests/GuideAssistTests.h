0dbe27366488f2d4e63f60b39d06c9e3ce4bc4bd
xory <xory001@163.com>
2012-04-09 23:05:58 +0800

    init version

---
+++ GuideAssistTests/GuideAssistTests.h
@@ -0,0 +1,17 @@
+//
+//  GuideAssistTests.h
+//  GuideAssistTests
+//
+//  Created by 朱 起伟 on 11-8-21.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface GuideAssistTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ GuideAssistTests/GuideAssistTests.m
@@ -0,0 +1,33 @@
+//
+//  GuideAssistTests.m
+//  GuideAssistTests
+//
+//  Created by 朱 起伟 on 11-8-21.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "GuideAssistTests.h"
+
+
+@implementation GuideAssistTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GuideAssistTests");
+}
+
+@end
