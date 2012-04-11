93636d9276e7cae372abf121c431f50416d1f055
xory <xory001@163.com>
2012-04-11 15:07:00 +0800

    test

---
+++ GuideAssist/CMainItinerary.m
@@ -0,0 +1,14 @@
+//
+//  CMainItinerary.m
+//  GuideAssist
+//
+//  Created by 朱 起伟 on 12-4-11.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CMainItinerary.h"
+
+
+@implementation CMainItinerary
+
+@end
